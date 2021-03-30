dfa31de7e02c5c1007c51ada7466618c79ef9cde
mzdase902 <mzdase902@gmail.com>
2021-03-30 16:52:19 +0300

    lab2

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+let a = prompt('a', 0);
+let a = prompt('b', 0)
+let result = (a + b < 4) ? "Мало" : "Много";
+
+alert(result);
