5a0cd4b57e14311d9ab9991f531e1e6b13a4ffb2
mzdase902 <mzdase902@gmail.com>
2021-03-30 19:13:39 +0300

    lab_js_2

--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@
 //   result = 'Много';
 // }
 
-let a = prompt('a', 0);
-let a = prompt('b', 0)
-let result = (a + b < 4) ? "Мало" : "Много";
+let a = +prompt('a');
+let b = +prompt('b');
+let result = (a + b < 4) ? 'Мало' : 'Много';
 
 alert(result);
+
+// let message;
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+let login = prompt('login');
+let message = (login == 'Сотрудник') ? 'Привет' : (login == 'Директор') ? 'Здравствуйте' : (login == '') ? 'Нет логина' : '';
+alert(message);
